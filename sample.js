ac3ab99e2c5719176ab454018092b4ecac99bb00
surekha123-natteti <surekhanatteti@gmail.com>
2024-10-19 11:47:25 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.client').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,      
+    });
+  });
+
+
